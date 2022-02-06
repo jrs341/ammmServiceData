110b337bc436c7bbdea52d50817379f9b5f30320
unknown <jrs341@me.com>
2022-02-06 12:19:16 -0600

    added web pack build script

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// src/index.js
+import { initializeApp } from 'firebase/app';
+import { getFirestore, doc, getDoc } from 'firebase/firestore/lite';
+
+const firebaseApp = initializeApp({ /* config */ });
+const db = getFirestore(firebaseApp);
+
+async function loadCity(name) {
+  const cityDoc = doc(db, `cities/${name}`);
+  const snapshot = await getDoc(cityDoc);
+  return {
+    id: snapshot.id,
+    ...snapshot.data(),
+  };
+}
